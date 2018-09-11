9aab181fc5d1230b42d8e6d242382f2e96e23a33
Faldy Ikhwan Fadila <faldy.if@gmail.com>
2018-09-11 23:27:58 +0700

    Configure PHPUnit to use sqlite memory and implement migration in test file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,28 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations, DatabaseTransactions, WithFaker;
+
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+    /**
+     * Reset the migrations
+     */
+    public function tearDown()
+    {
+        $this->artisan('migrate:reset');
+        parent::tearDown();
+    }
 }
