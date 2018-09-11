54bd2d4afa4facb13d6a955a58220ea02458083b
Faldy Ikhwan Fadila <faldy.if@gmail.com>
2018-09-12 00:42:01 +0700

    Define event relationships to other tables in Event class

--- app/Event.php
+++ app/Event.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $guarded = [];
+
+    /**
+     * Get the location of the event object.
+     */
+    public function location()
+    {
+        return $this->belongsTo('App\Location');
+    }
+
+    /**
+     * Get the ticket types for the event object.
+     */
+    public function ticketTypes()
+    {
+        return $this->hasMany('App\TicketType');
+    }
 }
