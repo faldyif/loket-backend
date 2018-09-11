11a52ebda6952f4a672772a3db926dce944ad355
Faldy Ikhwan Fadila <faldy.if@gmail.com>
2018-09-11 23:29:17 +0700

    Add create (event and location) feature tests

---
+++ tests/Feature/EventTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class EventTest extends TestCase
+{
+    public function testCreateNewLocationAndEvent()
+    {
+        // create location
+        $data = [
+            'name' => 'Gedung ' . title_case($this->faker->words(2, true)),
+            'city' => $this->faker->city,
+            'address' => $this->faker->address,
+            'latitude' => $this->faker->latitude,
+            'longitude' => $this->faker->longitude
+        ];
+        $this->post(url('location/create'), $data)
+            ->assertStatus(201);
+
+        // create event
+        $data = [
+            'name' => title_case($this->faker->words('3', true)),
+            'description' => $this->faker->paragraph('5'),
+            'location_id' => 1,
+            'start_at' => $this->faker->dateTime(),
+            'end_at' => $this->faker->dateTime()
+        ];
+        $this->post(url('event/create'), $data)
+            ->assertStatus(201);
+    }
+}
---
+++ tests/Feature/LocationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LocationTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCreateLocation()
+    {
+        // create location
+        $data = [
+            'name' => 'Gedung ' . title_case($this->faker->words(2, true)),
+            'city' => $this->faker->city,
+            'address' => $this->faker->address,
+            'latitude' => $this->faker->latitude,
+            'longitude' => $this->faker->longitude
+        ];
+        $this->post(url('location/create'), $data)
+            ->assertStatus(201);
+    }
+}
